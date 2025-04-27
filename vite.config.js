a300804f5045f386220ce8c321559af1af1e5baf
Saishreyass <shreyassai56@gmail.com>
2025-04-27 21:35:39 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
+    react()
+    ],
+    base: process.env.VITE_BASE_PATH || '/mern-stack',
 })
